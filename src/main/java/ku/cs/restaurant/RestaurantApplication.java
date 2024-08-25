23682e9f90fb40b493bb291e2efccb4c87b77b83
WeeranutCK <weeranut.c@ku.th>
2024-08-25 22:32:49 +0700

    Fix folder issue

---
+++ src/main/java/ku/cs/restaurant/RestaurantApplication.java
@@ -0,0 +1,16 @@
+/*
+    6510405806
+    Weeranut Chayakul
+*/
+
+package ku.cs.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantApplication.class, args);
+    }
+}
---
+++ src/main/java/ku/cs/restaurant/controller/RestaurantController.java
@@ -0,0 +1,57 @@
+/*
+    6510405806
+    Weeranut Chayakul
+*/
+
+package ku.cs.restaurant.controller;
+
+import ku.cs.restaurant.entity.Restaurant;
+import ku.cs.restaurant.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class RestaurantController {
+
+    @Autowired
+    private RestaurantService service;
+
+    @GetMapping("/restaurant")
+    public List<Restaurant> getAllRestaurant() {
+        return service.getRestaurants();
+    }
+
+    @PostMapping("/restaurant")
+    public Restaurant create(@RequestBody Restaurant restaurant) {
+        return service.createRestaurant(restaurant);
+    }
+
+    @GetMapping("/restaurant/{id}")
+    public Restaurant getRestaurantById(@PathVariable UUID id) {
+        return service.getRestaurantById(id);
+    }
+
+    @PutMapping("/restaurant")
+    public Restaurant update(@RequestBody Restaurant restaurant) {
+        return service.update(restaurant);
+    }
+
+    @DeleteMapping("/restaurant/{id}")
+    public Restaurant delete(@PathVariable UUID id) {
+        return service.delete(id);
+    }
+
+    @GetMapping("/restaurant/name/{name}")
+    public Restaurant getRestaurantByName(@PathVariable String name) {
+        return service.getRestaurantByName(name);
+    }
+
+
+    @GetMapping("/restaurant/location/{location}")
+    public List<Restaurant> getRestaurantByLocation(@PathVariable String location) {
+        return service.getRestaurantByLocation(location);
+    }
+}
